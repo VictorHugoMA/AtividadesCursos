0f0e399397430a6790f913000cb2d27a2cbdf63f
VictorHugoMA <victor.alves1@ufu.br>
2021-09-16 11:03:20 -0300

    add testes

---
+++ HTML_CSS/ImersaoDev/Teste/Lista/lista.js
@@ -0,0 +1,8 @@
+
+var listaFilmes = ["Homem de Ferro", "Interstelar", "Homem Aranha"]
+listaFilmes.push("Vingadores Ultimato")
+
+
+for (let i = 0; i < listaFilmes.length; i++) {
+    document.write("<p>"+ listaFilmes[i] + "</p>")
+}
