c4725eb635e0e65195e4052a774c86c9608d9046
VictorHugoMA <victor.alves1@ufu.br>
2021-10-27 09:38:40 -0300

    add teste em R

---
+++ R/teste.R
@@ -0,0 +1,26 @@
+#teste em R
+
+#Numeros
+
+n = 10
+class(n)
+
+is.integer(n)
+
+num1 = 1.5
+num2 = 3
+
+soma = num1 + num2
+soma
+
+#Booleanos
+
+bool1 = TRUE; bool2 = FALSE
+bool1 & bool2
+bool1 | bool2
+!bool1
+
+
+#String
+nome <- "Victor"
+nome
