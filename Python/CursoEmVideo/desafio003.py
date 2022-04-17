456aa7c5b09824629ddec422cdf642cacdd1133c
VictorHugoMA <victor.alves1@ufu.br>
2022-04-17 12:40:06 -0300

    att desafio

--- Python/CursoEmVideo/desafio003.py
+++ Python/CursoEmVideo/desafio003.py
@@ -1,4 +1,8 @@
-num1 = input('Numero 1:')
-num2 = input('Numero 2:')
+num1 = int(input('Numero 1:'))
+num2 = int(input('Numero 2:'))
 
-print('A soma é:', int(num1) + int(num2))
+print('A soma é:', num1 + num2)
+#outra maneira de fazer
+print('A soma entre {} e {} é {}'.format(num1, num2, num1 + num2))
+
+print('A subtração é: {}'.format(num1-num2))
