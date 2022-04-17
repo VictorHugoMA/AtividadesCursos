9ba617b6d80a7df37807aea2322d5bc1ea1100b4
VictorHugoMA <victor.alves1@ufu.br>
2022-04-17 11:49:05 -0300

    add exercicios python

---
+++ Python/CursoEmVideo/desafio002.py
@@ -0,0 +1,5 @@
+dia = input('Digite o dia do seu nascimento: ')
+mes = input('Digite o mês do seu nascimento: ')
+ano = input('Digite o ano do seu nascimento: ')
+
+print(dia + '/' + mes + '/' + ano)
---
+++ Python/CursoEmVideo/desafio003.py
@@ -0,0 +1,4 @@
+num1 = input('Numero 1:')
+num2 = input('Numero 2:')
+
+print('A soma é:', int(num1) + int(num2))
