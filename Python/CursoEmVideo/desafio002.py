58ba080439f050d8751f4156c5af42536a88bbf7
VictorHugoMA <victor.alves1@ufu.br>
2022-04-18 10:30:17 -0300

    att diretório

--- Python/CursoEmVideo/desafio002.py
+++
@@ -1,5 +0,0 @@
-dia = input('Digite o dia do seu nascimento: ')
-mes = input('Digite o mês do seu nascimento: ')
-ano = input('Digite o ano do seu nascimento: ')
-
-print(dia + '/' + mes + '/' + ano)
--- Python/CursoEmVideo/desafio003.py
+++
@@ -1,8 +0,0 @@
-num1 = int(input('Numero 1:'))
-num2 = int(input('Numero 2:'))
-
-print('A soma é:', num1 + num2)
-#outra maneira de fazer
-print('A soma entre {} e {} é {}'.format(num1, num2, num1 + num2))
-
-print('A subtração é: {}'.format(num1-num2))
